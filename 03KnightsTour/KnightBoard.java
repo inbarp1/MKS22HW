200fa42a30c46e9a2159b7024597870305fd170f
Inbar Pe'er <inbaronet@gmail.com>
2017-02-14 12:33:16 -0500

    queenz

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -1,10 +1,29 @@
 public class KnightBoard{
-public KnightBoard(int startingRows,int startingCols) 
+    private int[][]board;
+    private int m;
+    private int n;
+    public KnightBoard(int startingRows,int startingCols){
+	board = new int[startingRows][startingCols];
+	m=startingRows;
+	n=startingCols
+    }
 
-public String toString() //blank if you never called solve or when there is no solution
 
-public void solve() 
+    public String toString(){} //blank if you never called solve or when there is no solution
 
-private boolean solveH(int r,int c)
+    public void solve(){
+	return solveH(0,0);
+    }
+  
+    private boolean solveH(int r,int c, int num){
+	if(count > m * n){
+	    return true;
+	}
+	if isOnGoodSpot(){
+		placeQueen();
+		//try possible moves
+		removeQueen();
+		return false;
+    }
 
 }
