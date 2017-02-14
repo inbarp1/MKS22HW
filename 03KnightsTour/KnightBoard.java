e9a99881556c2c046f495279d6545d0f87a9eb34
Inbar Pe'er <inbaronet@gmail.com>
2017-02-14 12:06:26 -0500

    knights

---
+++ 03KnightsTour/KnightBoard.java
@@ -0,0 +1,10 @@
+public class KnightBoard{
+public KnightBoard(int startingRows,int startingCols) 
+
+public String toString() //blank if you never called solve or when there is no solution
+
+public void solve() 
+
+private boolean solveH(int r,int c)
+
+}
