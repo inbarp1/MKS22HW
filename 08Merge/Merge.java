fb41890c8e07790a744656da4ad17b54401d94db
Dana Peer <dana@bioadmins-MacBook-Pro-2.local>
2017-03-26 19:19:17 -0400

    :)

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -3,7 +3,7 @@ public class Merge{
     public static void mergesort(int[]ary){
 	int[] right;
 	int[]left;
-	System.out.println(toString(ary));				    
+	//System.out.println(toString(ary));				    
 	if(ary.length<=1){
 	    //System.out.println("im the basecase!! and ive reached myself");
 	}
@@ -14,8 +14,8 @@ public class Merge{
 		 right = new int[arysize/2];
 	    }
 	    else{
-		left = new int[arysize/2] ;
-		right = new int[arysize/2-1];
+		left = new int[arysize/2+1] ;
+		right = new int[arysize/2];
 	    }
 	    for(int i =0; i<(arysize/2); i++){
 		left[i]=ary[i];
@@ -24,9 +24,9 @@ public class Merge{
 		}
 	    }
 	    //System.out.println("left:");
-	    //System.out.println(toString(right));
+	    // System.out.println(toString(left));
 	    // System.out.println("right:");
-	    //System.out.println(toString(right));
+	    // System.out.println(toString(right));
 	    mergesort(left);
 	    if(right.length==1){
 	    }
@@ -43,7 +43,7 @@ public class Merge{
 	int i=0;
 	int j=0;
         int x=0;
-	System.out.println(toString(ary));
+	//System.out.println(toString(ary));
 	while(i < left.length && j < right.length){
             if(left[i] < right[j]){
                 ary[x] = left[i];
@@ -55,8 +55,8 @@ public class Merge{
             }
             x++;
         }
-	System.out.println("up to" + x);
-	System.out.println(toString(ary));
+	//System.out.println("up to" + x);
+	//System.out.println(toString(ary));
         if(j < right.length){
             for(;j < right.length;j++,x++){
                 ary[x] = right[j];
@@ -77,21 +77,9 @@ public class Merge{
 	}
 	return b;
    }
-   public static void main(String[] args){
-      int [] ary = new int[9];
-		ary[0]=3;
-		ary[1]=8;
-		ary[2]=5;
-		ary[3]=5;
-		ary[4]=5;
-		ary[5]=8;
-		ary[6]=2;
-		ary[7]=8;
-		ary[8]=6;
-		System.out.println(toString(ary));
-		mergesort(ary);
-		System.out.println(toString(ary));
-   }
+    public static void main(String[] args){
+       
+    }
 }
 	
 		
