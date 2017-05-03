9d1b0fb1b2d7578f2cca113f0aa934dba83f5f81
Inbar Pe'er <inbaronet@gmail.com>
2017-05-03 12:27:19 -0400

    tada

---
+++ 13Heaps/MyHeap.java
@@ -0,0 +1,17 @@
+import java.util.**
+    public class MyHeap{
+	private String[] maHeap;
+	public MyHeap(){
+	    maHeap= new Array<String>
+	    
+	    
+	}
+	public void add(){
+	}
+	public void remove(){
+	}
+	public void peek(){
+	}
+	
+
+    }
