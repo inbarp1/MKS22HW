77adb072d707b9753f6b6732fe8d73c09e6361bc
Inbar Pe'er <inbaronet@gmail.com>
2017-05-16 12:15:52 -0400

    done with location I believe

--- 15MazeFrontier/Location.java
+++ 15MazeFrontier/Location.java
@@ -2,11 +2,55 @@ public class Location implements Comparable<Location>{
     private int distToGoal,row, col, distToStart;
     private Location previous;
     private boolean aStar; 
-    public Location(int r, int c, Location p, int dts, int dtg){
+    public Location(int r, int c, Location p, int dts, int dtg boolean imAstar){
 	row = r;
 	col = c; 
 	previous = p;
 	distToStart= dts;
-	distToGoal; dtg; 
+	distToGoal=dtg;
+	aStar= imAstar;
     }
+    public boolean isAStar(){
+	return aStar;
+    }
+    public int getRow(){
+	return row;
+    }
+    public int getColumn(){
+	return col;
+    }
+    public int getDistToGoal(){
+	return distToGoal;
+    }
+    public int getDistToStart(){
+	return distToStart;
+    }
+    public Location getPrevious(){
+	return previous;
+    }
+    public int compareTo(Location other){
+	if(isAStar){
+	    int totalD= (this.getPrevious()).getDistanceToGoal()+ this.getDistanceToGoal();
+	    int totalDOther=(other.getPrevious()).getDistanceToGoal()+ other.getDistanceToGoal();
+	    if(totalD>totalDOther){
+		return 1;
+	    }
+	    if(totalD==totalDOther){
+		return 0;
+	    }
+	    return -1;
+	}
+	else{
+	    if(this.getDistToGoal()>other.getDistToGoal()){
+		return 1;
+	    }
+	    if(this.getDistToGoal()==other.getDistToGoal()){
+		return 0;
+	    }
+	    return -1;
+	}
+    }
+	
+    
+}
 	
