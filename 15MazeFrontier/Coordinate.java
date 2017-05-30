a05e3980284d11ef77d8e1471dbe2e1fc9533aa7
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2017-05-30 12:10:23 -0400

    :)

--- 15MazeFrontier/Coordinate.java
+++
@@ -1,8 +0,0 @@
-public class Coordinate {
-    int xcor, ycor;
-    
-    public Coordinate(int xcoordinate, int ycoordinate){
-	xcor=xcoordinate;
-	ycor=ycoordinate;
-    }
-}
--- 15MazeFrontier/Frontier.java
+++
@@ -1,5 +0,0 @@
-public interface Frontier{
-    public void add (Location loc);
-    public Location next();
-    public int size();
-}
